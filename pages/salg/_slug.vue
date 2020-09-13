c60aed022ed9a72cef3a53f36eaf8055663b6b9b
Rasmus Jürs <rasmus@jurs.me>
2020-09-13 22:18:58 +0200

    Add sale

--- pages/salg/_slug.vue
+++ pages/salg/_slug.vue
@@ -3,10 +3,6 @@
     <article class="max-w-5xl px-4 mx-auto mt-12 sm:px-6 lg:px-8">
       <h1 class="text-6xl font-black tracking-tight">{{ item.title }}</h1>
 
-      <PostMeta :author-data="item.author" :date="item.date" />
-
-      <img :src="item.thumbnail" alt="" class="w-full mt-6 rounded-lg" />
-
       <div
         class="mx-auto mt-12 prose-sm prose sm:prose lg:prose-lg xl:prose-2xl"
         v-html="$md.render(item.body)"
